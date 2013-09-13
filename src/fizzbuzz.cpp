2e47e5d7c4b2e16bfcc7faae3f1b87afe650eb72
kawasakitoshiya <kawasakitoshiya@gmail.com>
2013-09-14 00:09:24 +0900

    FizzBuzz::say says Fizz if given value is multiple of 3

--- src/fizzbuzz.cpp
+++ src/fizzbuzz.cpp
@@ -6,6 +6,9 @@ FizzBuzz::FizzBuzz(){
 }
 
 std::string FizzBuzz::say(int num){
+    
+    if(num%3 == 0) return "Fizz";
+    
     std::ostringstream stream;
     stream << num;
     std::string answer_str = stream.str();
